e8e74ce728c701f53503727dc78b4e2f99940f69
m-4morim <m.4morim@outlook.com>
2023-05-17 09:50:21 -0300

    :lipstick: ci: Importando imagens menu

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import home from '../../assets/icones/home-ativo.png'
+import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
+import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
+import novas from '../../assets/icones/novas-inativo.png'
+import supreendaMe from '../../assets/icones/supreenda-me-inativo.png'
 
 export default function Menu() {
     return (
