6912635f6aa6f4152b9f37df6a1eb456de53cd4d
m-4morim <m.4morim@outlook.com>
2023-05-17 09:37:16 -0300

    :bricks: ci: Construindo menu

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function Menu() {
     return (
-        <div>Menu</div>
+        <nav>
+            <ul>
+                <li>
+                    <img src='' alt='' />
+                    <a href='/'>Início</a>
+                </li>
+                <li>
+                    <img src='' alt='' />
+                    <a href='/'>Mais curtidas</a>
+                </li>
+                <li>
+                    <img src='' alt='' />
+                    <a href='/'>Mais vistas</a>
+                </li>
+                <li>
+                    <img src='' alt='' />
+                    <a href='/'>Novas</a>
+                </li>
+                <li>
+                    <img src='' alt='' />
+                    <a href='/'>Surpreenda-me</a>
+                </li>
+            </ul>
+        </nav>
     )
 }
