6ae4dc897d93118ebd2708650057ad05d7232b3a
m-4morim <m.4morim@outlook.com>
2023-05-20 22:34:47 -0300

    :bricks: ci: Iniciando banner

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -5,7 +5,15 @@ export default function PaginaInicial() {
     return (
         <>
             <Cabecalho />
-            <Menu />
+            <main>
+                <section>
+                    <Menu />
+                    <div>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
