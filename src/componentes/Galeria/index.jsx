6fa230488f32f8f8b54f6e7afcfd2cf368864287
m-4morim <m.4morim@outlook.com>
2023-06-09 21:21:11 -0300

    :package: build: Importando componente tags

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Tags from '../Tags'
 
 export default function Galeria() {
   return (
     <section>
       <h2>Navegue pela galeria</h2>
+      <Tags />
       <ul></ul>
     </section>
   )
