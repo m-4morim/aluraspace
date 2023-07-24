419bdee97fa189385fd2426dcf3e6cbea79c7964
m-4morim <m.4morim@outlook.com>
2023-07-24 20:09:40 -0300

    :heavy_plus_sign: build: Importando styles e fotos

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Tags from '../Tags'
+import styles from './Galeria.module.scss'
+import fotos from './fotos.json'
 
 export default function Galeria() {
   return (
-    <section>
+    <section className={styles.galeria}>
       <h2>Navegue pela galeria</h2>
       <Tags />
-      <ul></ul>
+      <ul className={styles.galeria__lista}></ul>
     </section>
   )
 }
