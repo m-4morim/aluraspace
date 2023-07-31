0745fbc80281f21a292b7abfd748e2a05f2e535e
m-4morim <m.4morim@outlook.com>
2023-07-31 19:30:04 -0300

    :construction: Importando img

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Tags from '../Tags'
 import styles from './Galeria.module.scss'
 import fotos from './fotos.json'
+import open from './open.png'
+import favorito from './favorito.png'
 
 export default function Galeria() {
   return (
@@ -9,7 +11,7 @@ export default function Galeria() {
       <h2>Navegue pela galeria</h2>
       <Tags />
       <ul className={styles.galeria__lista}>
-        {fotos.map((foto)=>{
+        {fotos.map((foto) => {
           return(
             <li key={foto.id} className={styles.galeria__card}>
               <img
